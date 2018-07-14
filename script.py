bb43e130a18bf104fd56cd2003597a6ae5f2b1ff
George Ridgway <youradhear@localhost.localdomain>
2018-07-14 09:28:57 +0100

    sample wallpers added

--- script.py
+++ script.py
@@ -1,7 +1,20 @@
+import sys
 
 def main():
-    print('hello world?')
+    #if the user wants to apply the wallpapers form an alternate dir
+    if (len(sys.argv) == 2):
+        checkAlternateLDir(sys.argv[1])
+    else:
+        applyWallpaper("")
 
-#needed to call main aparently
+
+def checkAlternateLDir(filePath):
+    print("Am I actually getting called?" + filePath)
+
+def applyWallpaper(filePath):
+    #If the user wants to use the default wallpapers included
+    if not filePath:
+        print("temp")
+
 if __name__ == '__main__':
-    main();
+    main()
